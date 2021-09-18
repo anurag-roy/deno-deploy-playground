dfcc0bf30b06378b4f1c0091a91b9d6835d62702
Anurag Roy <anurag@2pirad.com>
2021-09-18 16:43:24 +0530

    Read from file and respond

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@ import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
 const app = new Application();
 const router = new Router();
 
-router.get('/', (context) => {
-  context.response.body = 'Hello World!';
-});
+router
+  .get('/', (context) => {
+    context.response.body = 'Hello World!';
+  })
+  .get('pokemon', async (context) => {
+    const allPokemon = await Deno.readFile('../data/pokemon.json');
+    context.response.body = allPokemon[0];
+  });
 
 app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen({ secure: true });
+app.listen({ port: 80 });
