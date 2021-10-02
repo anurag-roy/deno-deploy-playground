9521f3bb74b8971745dee9b1d9d595ad29544f05
Anurag Roy <anurag@2pirad.com>
2021-10-02 15:49:33 +0530

    Test params

--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,29 @@ const router = new Router();
 const allPokemon: Pokemon[] = JSON.parse(await Deno.readTextFile('data/pokemon.json'));
 
 router
-  .get('/', (context) => {
-    context.response.body = 'Hello World!';
-  })
-  .get('/pokemon', (context) => {
-    context.response.body = allPokemon.map((p) => ({
-      id: p.id,
-      name: p.name.english,
-      type: p.type.join('/'),
-    }));
-  })
-  .get('/pokemon/:id', (context) => {
-    const pokemon = allPokemon.find((p) => p.id.toString() == context.params.id);
+    .get('/', async (context) => {
+        await context.send({
+            root: `${Deno.cwd()}/src/views`,
+            index: 'index.html',
+        });
+    })
+    .get('/pokemon', (context) => {
+        console.log('Params for this request', context.params);
+        context.response.body = allPokemon.map((p) => ({
+            id: p.id,
+            name: p.name.english,
+            type: p.type.join('/'),
+        }));
+    })
+    .get('/pokemon/:id', (context) => {
+        const pokemon = allPokemon.find((p) => p.id.toString() == context.params.id);
 
-    if (pokemon) {
-      context.response.body = pokemon;
-    } else {
-      context.throw(404, 'Pokemon not found!');
-    }
-  });
+        if (pokemon) {
+            context.response.body = pokemon;
+        } else {
+            context.throw(404, 'Pokemon not found!');
+        }
+    });
 
 app.use(oakCors());
 app.use(router.routes());
